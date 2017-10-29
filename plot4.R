06e5ec1f9f80b5faedb6fd53d28a57353dc68f06
pradeep-thawani <pradeepthawani07@gmail.com>
2017-10-29 19:17:29 +0530

    commit assignments..

--- plot4.R
+++ plot4.R
@@ -10,12 +10,12 @@ png(file = "plot4.png")
 par(mfrow = c(2,2))
 plot(powerCons$Date, powerCons$Global_active_power, type = "l", ylab = "Global Active Power", xlab = "")
 powerCons$Voltage <- as.numeric(levels(powerCons$Voltage))[powerCons$Voltage]
-plot(powerCons$Date, powerCons$Voltage, type = "l", ylab = "Voltage", xlab = "")
+plot(powerCons$Date, powerCons$Voltage, type = "l", ylab = "Voltage", xlab = "datetime")
 plot(powerCons$Date, powerCons$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")
 lines(powerCons$Date, powerCons$Sub_metering_2, type = "l", col = "red")
 lines(powerCons$Date, powerCons$Sub_metering_3, type = "l", col = "blue")
 legend("topright", lwd = 1, bty = "n", col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 powerCons$Global_reactive_power <- as.numeric(levels(powerCons$Global_reactive_power))[powerCons$Global_reactive_power]
-plot(powerCons$Date, powerCons$Global_reactive_power, type = "l", ylab = "Voltage", xlab = "")
+plot(powerCons$Date, powerCons$Global_reactive_power, type = "l", ylab = "Voltage", xlab = "datetime")
 
 dev.off()
